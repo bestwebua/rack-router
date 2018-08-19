6df549d4a9e558fd86da827887af1c588a6f69e8
Vladislav Trotsenko <admin@bestweb.com.ua>
2018-08-19 22:54:54 +0300

    Complete Router#initialize tests

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -3,21 +3,46 @@ require 'spec_helper'
 class Router
   RSpec.describe Router do
     describe '#initialize' do
-      let(:router_with_block) do
-        Router.new do
-          get '/', to: :home
-          post '/', to: :home
-        end
+      let(:router) { subject }
+      let(:get_post_stub) do
+        allow(router).to receive(:get)
+        allow(router).to receive(:post)
       end
 
       context 'without block' do
-        specify { expect(subject.resolver).to be_an_instance_of(Resolver) }
+        before do
+          get_post_stub
+          router
+        end
+
+        after { router }
+
+        specify { is_expected.not_to have_received(:get) }
+        specify { is_expected.not_to have_received(:post) }
       end
 
       context 'with block' do
-        specify do
-          expect(router_with_block.resolver.routes.keys).to eq(Constant::REQUEST_METHODS.split)
+        let(:method_calls) do
+          lambda do
+            get '/', to: :home
+            post '/', to: :home
+          end
         end
+
+        before do
+          get_post_stub
+          router.instance_exec(&method_calls)
+        end
+
+        after { router.instance_exec(&method_calls) }
+
+        specify { is_expected.to have_received(:get) }
+        specify { is_expected.to have_received(:post) }
+      end
+
+      describe '#resolver' do
+        specify { expect(router.methods).to include(:resolver, :resolver=) }
+        specify { expect(router.resolver).to be_an_instance_of(Resolver) }
       end
     end
   end
